8bb1b628e9c811e08ab742d5d9a1c0669d59fa6b
Thioubado <seckndanane16@gmail.com>
2025-06-03 21:29:37 +0000

    S'entrainer - Lire un entier

---
+++ maison des jeux/Infatigables.py
@@ -0,0 +1,3 @@
+vitesse = int(input())
+distance = vitesse * 33600
+print(distance)
---
+++ maison des jeux/S_entrainer_lire_un_entier.py
@@ -0,0 +1,4 @@
+entier = int(input())
+phrase = "Je dois courir à mon rythme pour pouvoir courir longtemps. "
+for _ in range(entier):
+    print(phrase)
