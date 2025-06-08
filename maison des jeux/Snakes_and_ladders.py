12211ef61e12be483f73ea3628f891091d0046a2
Thioubado <seckndanane16@gmail.com>
2025-06-08 18:44:31 +0000

    Snakes and ladders

---
+++ maison des jeux/Snakes_and_ladders.py
@@ -0,0 +1,25 @@
+maximum = int(input())
+minimum = int(input())
+
+for i in range(maximum, minimum - 1, -1):
+    print(i)
+
+
+"""for i in range(maximum, minimum, -1):
+    print(i)"""
+
+"""for i in range(maximum, minimum,-1):
+    i = maximum - 1
+    print (i)"""
+
+
+
+"""for i in range(minimum , maximum):
+    if(minimum <= maximum):
+        o = minimum + i
+    print(reversed(o))
+print(maximum)"""
+
+
+"""interne = minimum + i
+    print(interne)"""
