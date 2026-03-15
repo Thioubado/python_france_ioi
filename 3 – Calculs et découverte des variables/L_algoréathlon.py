8bfa8d735540edc1766622f4e3ccf4a803d2b166
Thioubado <seckndanane16@gmail.com>
2026-03-15 22:47:16 +0000

    L'algoréathlon

---
+++ 3 – Calculs et découverte des variables/L_algoréathlon.py
@@ -0,0 +1,8 @@
+distance_parcourue_premier_jour = 2 + 34 + 6
+distance_parcourue_deuxieme_jour = distance_parcourue_premier_jour * 2
+distance_parcourue_troisieme_jour = distance_parcourue_premier_jour * 3
+
+print(distance_parcourue_premier_jour, end=" ")
+print(distance_parcourue_deuxieme_jour, end=" ")
+print(distance_parcourue_troisieme_jour, end=" ")
+
