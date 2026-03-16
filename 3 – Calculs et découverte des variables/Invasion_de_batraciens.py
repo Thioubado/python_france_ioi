76f88809391d4409e7f5117f26b94a25441b8584
Thioubado <seckndanane16@gmail.com>
2026-03-16 00:40:41 +0000

    Invasion de batraciens

---
+++ 3 – Calculs et découverte des variables/Invasion_de_batraciens.py
@@ -0,0 +1,9 @@
+nb_crapauds = 1337
+for semaine in range(12):
+    semaine += 1
+    nb_crapauds *= 2 
+    print(f"semaine {semaine}: {nb_crapauds}")
+print(nb_crapauds)
+"""    semaine1 = 2
+    semaine2 = 4
+    semaine3 = 6"""
