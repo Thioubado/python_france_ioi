00e1ba8442bfed268b986210cc6ed91ba2774dbb
Thioubado <seckndanane16@gmail.com>
2026-03-24 22:14:12 +0000

    Retraite spirituelle

---
+++ 4 – Lecture de l'entrée/Retraite_spirituelle.py
@@ -0,0 +1,7 @@
+nombre_de_jours_de_la_randonnée = int(input())
+
+nombre_incantation_par_jour = 16 * 60 * 60
+
+nombre_de_fois_que_l_incantation_est_répétée = nombre_de_jours_de_la_randonnée * nombre_incantation_par_jour
+
+print(nombre_de_fois_que_l_incantation_est_répétée)
