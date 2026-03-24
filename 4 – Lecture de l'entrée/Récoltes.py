3b7f7913043570d4be5c2c9cbe4ef24df48f9f43
Thioubado <seckndanane16@gmail.com>
2026-03-24 22:04:11 +0000

    Récoltes

---
+++ 4 – Lecture de l'entrée/Récoltes.py
@@ -0,0 +1,7 @@
+longueur_du_côté_d_un_champ_carré = int(input())
+
+surface = longueur_du_côté_d_un_champ_carré * longueur_du_côté_d_un_champ_carré
+
+rendement_au_metre_carre = 23
+masse = surface * rendement_au_metre_carre
+print(masse)
