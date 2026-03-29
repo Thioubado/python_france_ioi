3a51e794bdfb27cb0cab03c567b89bf0859667d1
Thioubado <seckndanane16@gmail.com>
2026-03-29 10:35:35 +0000

    Exercice 4 — Suite géométrique

---
+++ 4 – Lecture de l'entrée/Claude exo/Exercice_4_Suite_géométrique.py
@@ -0,0 +1,20 @@
+# Exercice 4 — Suite géométrique
+"""nombre = int(input())
+
+n = 1
+#print(n)
+for element in range(2, nombre+1):
+    #print (element)
+    n = n *2 
+    print(n)"""
+
+
+# Exercice 4 — Suite géométrique
+nombre = int(input())
+
+n = 1
+
+print(n)
+for _ in range(1, nombre):
+    n = n *2 
+    print(n)
