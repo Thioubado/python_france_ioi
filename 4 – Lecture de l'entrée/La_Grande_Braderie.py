810d16fd115296ae9bbcb58c30cecfe36436c5fd
Thioubado <seckndanane16@gmail.com>
2026-03-30 21:53:15 +0000

    La Grande Braderie

---
+++ 4 – Lecture de l'entrée/La_Grande_Braderie.py
@@ -0,0 +1,38 @@
+# La Grande Braderie
+
+"""positionDepart = int(input())
+largeurEmplacement = int(input())
+nbVendeurs = int(input())
+
+resultat = positionDepart + largeurEmplacement
+print(positionDepart)
+print(resultat)
+
+for element in range(nbVendeurs -1):
+    #resultat = positionDepart + largeurEmplacement
+    resultat = resultat + largeurEmplacement
+    print(resultat)"""
+
+
+positionDepart = int(input())
+largeurEmplacement = int(input())
+nbVendeurs = int(input())
+
+resultat = positionDepart
+#print(positionDepart)
+print(resultat)
+
+for element in range(nbVendeurs):
+    #resultat = positionDepart + largeurEmplacement
+    resultat = resultat + largeurEmplacement
+    print(resultat)
+
+"""positionDepart = int(input())
+largeurEmplacement = int(input())
+nbVendeurs = int(input())
+
+position = positionDepart
+print(position)
+for _ in range(nbVendeurs):
+    position += largeurEmplacement
+    print(position)"""
