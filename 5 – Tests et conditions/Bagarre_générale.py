001fe6bd4b3202ea8e258d1ab270d8c29637f41d
Thioubado <seckndanane16@gmail.com>
2026-04-01 01:15:36 +0000

    Bagarre générale

---
+++ 5 – Tests et conditions/Bagarre_générale.py
@@ -0,0 +1,8 @@
+# Bagarre générale
+champ_des_Arignon = int(input())
+champ_des_Evaran = int(input())
+if champ_des_Arignon - champ_des_Evaran > 10:
+    print("La famille Arignon a un champ trop grand")
+elif champ_des_Evaran - champ_des_Arignon > 10:
+    print("La famille Evaran a un champ trop grand")
+
