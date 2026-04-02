32794f9b857957035ebf925a900f2e3124a952bf
Thioubado <seckndanane16@gmail.com>
2026-04-02 00:18:02 +0000

    Mot de passe du village

---
+++ 5 – Tests et conditions/Mot_de_passe_du_village.py
@@ -0,0 +1,8 @@
+# Mot de passe du village
+code_fourni_par_l_utilisateur = int(input())
+bon_code = 64741
+
+if code_fourni_par_l_utilisateur != bon_code :
+    print("Allez-vous en ! ")
+else:
+    print("Bon festin ! ")
