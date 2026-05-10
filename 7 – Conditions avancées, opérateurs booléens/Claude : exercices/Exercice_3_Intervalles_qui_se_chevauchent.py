ed86588a41cf4262b2ee75a2718b7875aa4beef7
Thioubado <seckndanane16@gmail.com>
2026-05-10 14:34:54 +0000

    Exercice 3 — Intervalles qui se chevauchent

---
+++ 7 – Conditions avancées, opérateurs booléens/Claude : exercices/Exercice_3_Intervalles_qui_se_chevauchent.py
@@ -0,0 +1,12 @@
+# Exercice 3 — Intervalles qui se chevauchent
+
+a1 = int(input())
+b1 = int(input())
+
+a2 = int(input())
+b2 = int(input())
+
+if a1 < b2 and a2 < b1 :
+    print("OUI")
+else:
+    print("NON")
