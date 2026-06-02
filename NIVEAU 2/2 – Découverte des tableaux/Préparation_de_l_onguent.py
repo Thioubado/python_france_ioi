9b93debc48054c85b13b2b8ef619a471f690650b
Thioubado <seckndanane16@gmail.com>
2026-06-02 22:47:41 +0000

    Préparation de l'onguent

---
+++ NIVEAU 2/2 – Découverte des tableaux/Préparation_de_l_onguent.py
@@ -0,0 +1,31 @@
+# Préparation de l'onguent
+
+"""entier = int(input())
+
+for i in range(10):
+    if entier == i == 0:
+        print(500)
+    elif entier == i == 1:
+        print(180)
+    elif entier == i == 2:
+        print(650)
+    elif entier == i == 3:
+        print(25)
+    elif entier == i == 4:
+        print(666)
+    elif entier == i == 5:
+        print(42)
+    elif entier == i == 6:
+        print(421)
+    elif entier == i == 7:
+        print(1)
+    elif entier == i == 8:
+        print(370)
+    elif entier == i == 9:
+        print(211)"""
+
+entier = int(input())
+
+quantites = [500, 180, 650, 25, 666, 42, 421, 1, 370, 211]
+
+print(quantites[entier])
