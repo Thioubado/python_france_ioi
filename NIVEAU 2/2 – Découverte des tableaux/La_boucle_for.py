7c5d5ceb7aba44d9955d36182c6dde858a589b02
Thioubado <seckndanane16@gmail.com>
2026-06-02 22:18:22 +0000

    La boucle « for »

---
+++ NIVEAU 2/2 – Découverte des tableaux/La_boucle_for.py
@@ -0,0 +1,16 @@
+# La boucle « for »
+for loop in range(10):
+    print(loop)
+
+for nombre in range(0,10, 1):
+    print(nombre, end=" ")
+
+for nombre in range(10, 21, 1):
+    print(nombre)
+
+for i in range(100, 121, 2):
+    print(i, end=" ")
+
+
+for nombre in range(20,-20, -5):
+    print(nombre, end=" ")
