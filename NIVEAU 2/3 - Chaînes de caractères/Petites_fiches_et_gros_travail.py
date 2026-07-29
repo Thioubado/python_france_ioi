2789bfd78e175a0743eec1b8dccfcc98089502e9
Thioubado <seckndanane16@gmail.com>
2026-07-29 22:11:45 +0000

    Petites fiches et gros travail

---
+++ NIVEAU 2/3 - Chaînes de caractères/Petites_fiches_et_gros_travail.py
@@ -0,0 +1,22 @@
+# Petites fiches et gros travail
+nombre_de_livres = 6
+
+for livre in range(nombre_de_livres):
+    nom_de_l_auteur = input()
+    titre_du_livre = input()
+
+    print(titre_du_livre)
+    print(nom_de_l_auteur)
+
+"""liste_des_livres = []
+for livre in range(nombre_de_livres):
+    nom_de_l_auteur = input()
+    titre_du_livre = input()
+
+    liste_des_livres.append(nom_de_l_auteur)
+    liste_des_livres.append(titre_du_livre)
+#print(liste_des_livres)
+
+for i in range(nombre_de_livres):
+    print(liste_des_livres[i * 2 + 1])
+    print(liste_des_livres[i * 2])"""
